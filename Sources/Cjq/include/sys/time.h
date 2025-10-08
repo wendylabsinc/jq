8bd45082fc6b0733d9d7d7cbe415faa15bbc62cf
Zamderax <zamderax@gmail.com>
2025-10-08 11:03:50 +0200

    Windows CI: implement gettimeofday via _ftime64; drop windows.h/winsock includes to avoid conflicts

--- Sources/Cjq/include/sys/time.h
+++ Sources/Cjq/include/sys/time.h
@@ -3,8 +3,8 @@
 #define JQ_SWIFT_SYS_TIME_H
 
 #ifdef _WIN32
-#include <windows.h>
 #include <time.h>
+#include <sys/timeb.h>
 
 // Define timeval if not defined and prevent later winsock redefinition
 #ifndef _TIMEVAL_DEFINED
@@ -17,14 +17,10 @@ struct timeval {
 
 static inline int gettimeofday(struct timeval *tv, void *tz_unused) {
   (void)tz_unused;
-  FILETIME ft;
-  GetSystemTimeAsFileTime(&ft);
-  ULARGE_INTEGER uli;
-  uli.LowPart = ft.dwLowDateTime;
-  uli.HighPart = ft.dwHighDateTime;
-  unsigned long long usec = (uli.QuadPart - 116444736000000000ULL) / 10ULL; // to microseconds since Unix epoch
-  tv->tv_sec = (long)(usec / 1000000ULL);
-  tv->tv_usec = (long)(usec % 1000000ULL);
+  struct __timeb64 tb;
+  _ftime64(&tb);
+  tv->tv_sec = (long)tb.time;
+  tv->tv_usec = (long)(tb.millitm * 1000);
   return 0;
 }
 
